7f0a81599a32db0f9740e70bd7daf2c8f47930e2
Henning Klatt <henning.klatt@web.de>
2016-10-18 19:28:33 +0200

    Debug AN

--- server/server.py
+++ server/server.py
@@ -145,4 +145,4 @@ def sensors():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=8081, debug=True, threaded=True)
