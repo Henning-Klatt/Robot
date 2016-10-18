86e0efce1f0634696a94782c5ea6d08f2f90edc3
Henning Klatt <henning.klatt@web.de>
2016-10-18 20:16:27 +0200

    auf fixes

--- web/index.php
+++ web/index.php
@@ -325,25 +325,6 @@ if(!$_SESSION['logged_in'])
         <div id="page-wrapper">
           <script>
 
-          window.addEventListener("keydown", keysPressed, false);
-          window.addEventListener("keyup", keysReleased, false);
-
-          function startTime() {
-            var today = new Date();
-            var h = today.getHours();
-            var m = today.getMinutes();
-            var s = today.getSeconds();
-            m = checkTime(m);
-            s = checkTime(s);
-            document.getElementById('clock').innerHTML =
-            h + ":" + m + ":" + s;
-            var t = setTimeout(startTime, 500);
-          }
-          function checkTime(i) {
-            if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-            return i;
-          }
-
           function clearselection() {
             document.getElementById("dashboard_menu").className = "";
             document.getElementById("remote_menu").className = "";
