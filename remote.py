98fdd5c8f35927fc036e8bfbc367e1f6f7f866de
Henning Klatt <henning.klatt@web.de>
2017-02-18 16:44:54 +0100

    PS3 Remote

--- remote.py
+++ remote.py
@@ -130,8 +130,8 @@ for btn in buf[:num_buttons]:
     button_map.append(btn_name)
     button_states[btn_name] = 0
 
-print '%d axes found: %s' % (num_axes, ', '.join(axis_map))
-print '%d buttons found: %s' % (num_buttons, ', '.join(button_map))
+print '%d axes found: %s', % (num_axes, ', '.join(axis_map))
+print '%d buttons found: %s', % (num_buttons, ', '.join(button_map))
 
 # Main event loop
 while True:
