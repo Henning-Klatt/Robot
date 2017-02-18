2b823a568577edfbc0b485b1c3f12df20e09eaac
Henning Klatt <henning.klatt@web.de>
2017-02-18 15:38:59 +0100

    PS3 Remote

--- remote.py
+++ remote.py
@@ -15,3 +15,20 @@ print "Buttons: ", j.get_numbuttons()
 print "Axis:    ", j.get_numaxes()
 print "Numhats: ", j.get_numhats()
 print "Numballs ", j.get_numballs()
+
+try:
+
+    while j.get_button(3) == 0:
+
+        pygame.event.pump()
+		speed= j.get_axis(13)
+		pwmout= speed * 1023
+
+		if j.get_axis(13) != 0.00:
+			print int(pwmout)
+
+
+		elif j.get_axis(13) == 0.00:
+except KeyboardInterrupt:
+	j.quit()
+	sys.exit()
