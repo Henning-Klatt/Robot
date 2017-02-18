bbedde9a78a35b4baa9514607f29c936430e8097
Henning Klatt <henning.klatt@web.de>
2017-02-18 18:25:15 +0100

    self.fix

--- server/server.py
+++ server/server.py
@@ -107,7 +107,7 @@ def sensors():
 
 
 if __name__ == '__main__':
-    server = multiprocessing.Process(target=app.run, args = (host=0.0.0.0, port=8081, debug=True, threaded=True))
+    server = multiprocessing.Process(target=app.run, args = (0.0.0.0, 8081, True, True))
     server.start()
     #app.run(host='0.0.0.0', port=8081, debug=True, threaded=True)
     PS3().listen
