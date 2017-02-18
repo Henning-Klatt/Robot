30cd9d9140e8b9b00a2ab4888c96a681fe711ae5
Henning Klatt <henning.klatt@web.de>
2017-02-18 18:11:37 +0100

    self.fix

--- server/action.py
+++ server/action.py
@@ -6,12 +6,13 @@ import serial
 class Action:
     def __init__(self):
         try:
-            self.arduino = serial.Serial('/dev/ttyACM0', 9600)
+            print "Hi"
         except:
             self.arduino = serial.Serial('/dev/ttyACM1', 9600)
 
     def moveServo(self, x, y):
+        arduino = serial.Serial('/dev/ttyACM0', 9600)
         print "Servo bewegt! ( x: " + str(x) + " | y: " + str(y) +" )"
-        self.arduino.write('1,1,' + str(x))
+        arduino.write('1,1,' + str(x))
         time.sleep(0.1)
-        self.arduino.write('2,1,' + str(y))
+        arduino.write('2,1,' + str(y))
