6ab3de11faebebd62fd79c8e464edf245a3c3bf8
Henning Klatt <henning.klatt@web.de>
2017-03-16 17:15:20 +0100

    Updated servo values

--- server/controller.py
+++ server/controller.py
@@ -127,7 +127,7 @@ class PS3:
                             if(button == "Lright"):
                                 moveServo(150, 90)
                             if(button == "Lleft"):
-                                moveServo(590, 90)
+                                moveServo(599, 90)
                             if(button == "Lup"):
                                 moveServo(90, 20)
                             if(button == "Ldown"):
