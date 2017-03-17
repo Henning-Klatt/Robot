c381a8b22568f20c80aca6454ecbad2720e64659
Henning Klatt <henning.klatt@web.de>
2017-03-17 19:56:56 +0100

    1 Joystick controll

--- server/controller.py
+++ server/controller.py
@@ -192,13 +192,13 @@ class PS3:
                                 print "Yminus: " + str(Yminus)
                                 print "======================"
                                 #Motor 1 Links
-                                moveMotor(2, Yplus + Xplus)
+                                moveMotor(2, Yplus + Xminus)
                                 #Motor 1 Rechts
-                                moveMotor(3, Yminus + Xminus)
+                                moveMotor(3, Yminus + Xplus)
                                 #Motor 2 Links
-                                moveMotor(4, Yminus + Xminus)
+                                moveMotor(4, Yminus + Xplus)
                                 #Motor 2 Rechts
-                                moveMotor(5, Yplus + Xplus)
+                                moveMotor(5, Yplus + Xminus)
 
                         if(axis != "unknown"):
                             print ("%s: %.3f" % (axis, fvalue))
