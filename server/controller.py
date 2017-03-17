487200aff8e7a32c67614ead5e613f771f4f8377
Henning Klatt <henning.klatt@web.de>
2017-03-17 21:06:24 +0100

    1 Joystick controll

--- server/controller.py
+++ server/controller.py
@@ -192,9 +192,9 @@ class PS3:
                                 print "Yminus: " + str(Yminus)
                                 print "======================"
                                 #Motor 1 Links
-                                moveMotor(2, abs(Xplus + Yplus))
+                                moveMotor(2, abs(Xplus - Yplus))
                                 #Motor 1 Rechts
-                                moveMotor(3, abs(Xminus + Yminus))
+                                moveMotor(3, abs(Xminus - Yminus))
                                 #Motor 2 Links
                                 moveMotor(4, abs(Xplus - Yplus))
                                 #Motor 2 Rechts
