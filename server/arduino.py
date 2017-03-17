bcae7b281143badb423ad0da224ba502444b770d
Henning Klatt <henning.klatt@web.de>
2017-03-17 16:19:31 +0100

    Testing new PWM method

--- server/arduino.py
+++ server/arduino.py
@@ -1,15 +1,19 @@
-import Adafruit_PCA9685
+#import Adafruit_PCA9685
+from pwm import PWM
 import time
 
-pwm = Adafruit_PCA9685.PCA9685()
-pwm.set_pwm_freq(200)
-pwm.set_pwm(0, 0, 300)
-pwm.set_pwm(1, 0, 300)
+#pwm = Adafruit_PCA9685.PCA9685()
+pwm = PWM(0x40)
+pwm.setPWMFreq(200)
+#pwm.set_pwm_freq(200)
+#pwm.set_pwm(0, 0, 300)
+#pwm.set_pwm(1, 0, 300)
 
 def moveServo(servo, value):
     print "Servo bewegt! ( Servo: " + str(servo) + " | Value: " + str(value) +" )"
-    pwm.set_pwm(servo, 0, value)
+    #pwm.set_pwm(servo, 0, value)
 
 def moveMotor(motor, value):
     print "Motor bewegt! ( Motor: " + str(motor) + " | Value: " + str(value) + " )"
-    pwm.set_pwm(motor, 0, value)
+    #pwm.set_pwm(motor, 0, value)
+    pwm.setPWM(motor, 0, value)
