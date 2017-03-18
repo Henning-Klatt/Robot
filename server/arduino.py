706141bbe7eca99a02c8273ef6d48af08a28085d
Henning Klatt <henning.klatt@web.de>
2017-03-18 10:45:30 +0100

    Controller

--- server/arduino.py
+++ server/arduino.py
@@ -1,16 +1,12 @@
-#import Adafruit_PCA9685
 from pwm import PWM
 import time
 
-#pwm = Adafruit_PCA9685.PCA9685()
 pwm = PWM(0x40)
-pwm.setPWMFreq(200)
-#pwm.set_pwm_freq(200)
-#pwm.set_pwm(0, 0, 300)
-#pwm.set_pwm(1, 0, 300)
+pwm.setPWMFreq(60)#Servo: 60 | DC Motor: 200
 
 def moveServo(servo, value):
     print "Servo bewegt! ( Servo: " + str(servo) + " | Value: " + str(value) +" )"
+    pwm.setPWM(servo, 0, value)
     #pwm.set_pwm(servo, 0, value)
 
 def moveMotor(motor, value):
