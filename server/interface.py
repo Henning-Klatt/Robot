0f83b417d7e5bff2665abc8b617be9f0b9ca8211
Henning Klatt <henning.klatt@web.de>
2017-03-19 10:05:44 +0100

    added debig information

--- server/interface.py
+++ server/interface.py
@@ -5,6 +5,7 @@ import time
 
 pwm = Adafruit_PCA9685.PCA9685()
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(5, GPIO.OUT)#M1 Links
 GPIO.setup(6, GPIO.OUT)#M1 Rechts
 GPIO.setup(13, GPIO.OUT)#M2 Links
@@ -27,12 +28,16 @@ def moveMotor(motor, value):
 def stopMotor(pin):
     if(pin == 1):
         GPIO.output(5, GPIO.LOW)
+        print "5 Low"
     if(pin == 2):
         GPIO.output(6, GPIO.LOW)
+        print "6 Low"
     if(pin == 3):
         GPIO.output(13, GPIO.LOW)
+        print "13 Low"
     if(pin == 4):
         GPIO.output(19, GPIO.LOW)
+        print "19 Low"
 
 def stopAll():
     GPIO.output(5, GPIO.LOW)
@@ -43,13 +48,17 @@ def stopAll():
 def startMotor(pin, speed):
     if(pin == 1):
         GPIO.output(5, GPIO.HIGH)
+        print "5 High"
         pwm.set_pwm(2, 0, speed)
     if(pin == 2):
         GPIO.output(6, GPIO.HIGH)
+        print "6 High"
         pwm.set_pwm(2, 0, speed)
     if(pin == 3):
         GPIO.output(13, GPIO.HIGH)
+        print "13 High"
         pwm.set_pwm(3, 0, speed)
     if(pin == 4):
         GPIO.output(19, GPIO.HIGH)
+        print "19 High"
         pwm.set_pwm(3, 0, speed)
