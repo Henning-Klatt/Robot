f670f244e0ab40756cad72f63a03ef114f55bc46
Henning Klatt <henning.klatt@web.de>
2017-05-08 17:56:31 +0200

    New method

--- Application/client.py
+++ Application/client.py
@@ -1,69 +1,32 @@
 import socket
-
 import pygame
-
 import sys
 
-
-
+host = "192.168.1.1"
 port=5000
-
-
-
-#create pygame screen
-
-screen = pygame.display.set_mode((800,600),0)
-
+screen = pygame.display.set_mode((320,240),0)
 
 
 while True:
-
-    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    s.bind(("",port)) # server is available on the whole network by setting host to ""
-
-    s.listen(1)
-
-    connection, addr = s.accept()
-
+    clientsocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect((host, port))
     received = []
-
-
-
     # loop .recv, it returns empty string when done, then transmitted data is completely received
-
     while True:
-
-        recvd_data = connection.recv(1440021)
-
+        #print("esperando receber dado")
+        recvd_data = clientsocket.recv(230400)
         if not recvd_data:
-
             break
-
         else:
-
             received.append(recvd_data)
 
-
-
     dataset = ''.join(received)
-
-    image = pygame.image.fromstring(dataset,(800,600),"RGB") # convert received image from string
-
-    #image = pygame.transform.scale(image,(800,600)) # scale image to 800*600
-
+    image = pygame.image.fromstring(dataset,(320,240),"RGB") # convert received image from string
     screen.blit(image,(0,0)) # "show image" on the screen
-
     pygame.display.update()
 
-
-
     # check for quit events
-
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
-
             pygame.quit()
-
             sys.exit()
--- Application/server.py
+++ Application/server.py
@@ -1,57 +1,24 @@
 import socket
-
 import pygame
-
 import pygame.camera
-
 import sys
-
 import time
 
-
-
-#host = "192.168.100.9"
-
-host = "192.168.1.64"
-
 port = 5000
-
-
-
-
-
 pygame.init()
 
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(("",port))
+serversocket.listen(1)
+
 pygame.camera.init()
-
-
-
-cam_list = pygame.camera.list_cameras() # list available cameras
-
-webcam = pygame.camera.Camera(cam_list[0],(800,600)) # use first camera in list and set resolution
-
-webcam.start() # start camera
-
-
+webcam = pygame.camera.Camera("/dev/video0",(320,240))
+webcam.start()
 
 while True:
-
-    image = webcam.get_image() # capture image
-
-    data = pygame.image.tostring(image,"RGB") # convert captured image to string, use RGB color scheme
-
-    #print sys.getsizeof(data) # in case somebody wants to know the size of the captured image
-
-
-
-    # prepare for connection to server
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP is used
-
-    s.connect((host, port))
-
-    s.sendall(data)
-
-    s.close()
-
-    time.sleep(0.1)
+        connection, address = serversocket.accept()
+        image = webcam.get_image() # capture image
+        data = pygame.image.tostring(image,"RGB") # convert captured image to string, use RGB color scheme
+        connection.sendall(data)
+        time.sleep(0.1)
+        connection.close()
