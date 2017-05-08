75c14c1afdbb7cca5135e0f393f42765464eb237
Henning Klatt <henning.klatt@web.de>
2017-05-08 18:20:13 +0200

    Less delay

--- Application/client.py
+++ Application/client.py
@@ -11,21 +11,18 @@ while True:
     clientsocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientsocket.connect((host, port))
     received = []
-    # loop .recv, it returns empty string when done, then transmitted data is completely received
     while True:
-        #print("esperando receber dado")
         recvd_data = clientsocket.recv(230400)
         if not recvd_data:
             break
         else:
             received.append(recvd_data)
 
-    dataset = ''.join(received)
-    image = pygame.image.fromstring(dataset,(320,240),"RGB") # convert received image from string
-    screen.blit(image,(0,0)) # "show image" on the screen
+    dataset = b''.join(v for v in received)
+    image = pygame.image.fromstring(dataset,(320,240),"RGB")
+    screen.blit(image,(0,0))
     pygame.display.update()
 
-    # check for quit events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
